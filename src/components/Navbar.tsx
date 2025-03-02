9c50b6e68527ae8794a1ae442094c639f6457d17
chandlergims <chandlergims@gmail.com>
2025-03-02 14:36:10 -0500

    Add X (Twitter) logo to navbar with link to trenchcompapp account

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,19 @@
 import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { SunIcon, MoonIcon } from '@heroicons/react/24/outline'
+// X Logo SVG component
+const XLogo = () => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    width="18" 
+    height="18" 
+    viewBox="0 0 24 24" 
+    fill="currentColor"
+    className="x-logo"
+  >
+    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+  </svg>
+)
 import '../styles/Navbar.css'
 
 function Navbar() {
@@ -36,6 +49,15 @@ function Navbar() {
           >
             Docs
           </Link>
+          <a 
+            href="https://x.com/trenchcompapp" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="social-link"
+            aria-label="Follow us on X (Twitter)"
+          >
+            <XLogo />
+          </a>
           <button 
             className="theme-toggle"
             onClick={toggleTheme}
